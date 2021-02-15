060af894b3baa82fc8ce05812bea6ffbf7823941
kwanghyeon <wjdrhkd456@gmail.com>
2021-02-15 23:01:51 +0900

    [update] /profile USER 권한없이 접근가능함 테스트

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -15,6 +15,7 @@ public class PostsApiController {
 
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        System.out.println(">>>>>>>뭐냐>>>>>>>>>>");
         return postsService.save(requestDto);
     }
 
